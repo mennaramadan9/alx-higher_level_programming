51d0a2348a3c30165387970b7789835e871c1340
mennaramadan9 <mennaramadan56@gmail.com>
2023-11-28 12:23:13 -0800

    edit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for tens in range(10):
     for ones in range(tens + 1, 10):
-        print("{:d}{:d}".format(tens, ones), end=", " if (tens, ones) != (8, 9) else "\n")
+        sep = ", " if (tens, ones) != (8, 9) else "\n"
+        print("{:d}{:d}".format(tens, ones), end=sep)
