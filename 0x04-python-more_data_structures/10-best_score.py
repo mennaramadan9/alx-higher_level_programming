8535cdd12f5bd5a5362eb7bba4d663693c3119d3
mennaramadan9 <mennaramadan56@gmail.com>
2023-12-06 11:45:29 -0800

    m

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,9 @@
 def best_score(a_dictionary):
     if a_dictionary:
         max_value = max(a_dictionary.values())
-        best_keys = [key for key, value in a_dictionary.items() if value == max_value]
+        best_keys = []
+        for key, value in a_dictionary.items():
+            if value == max_value:
+                best_keys.append(key)
         return best_keys[0] if best_keys else None
     return None
