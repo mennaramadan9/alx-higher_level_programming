cdc788cbd6a66b0592c752fc52985db6fa65b104
mennaramadan9 <mennaramadan56@gmail.com>
2024-06-18 05:03:36 -0700

    m

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  const n = parseInt(process.argv[2]);
+  for(let i = 0; i < n; i++)
+    console.log('C is fun');
+}
