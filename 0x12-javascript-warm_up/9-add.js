3431ecc6be8c3d9ffd8b94a3c13236d21c5e8bc0
mennaramadan9 <mennaramadan56@gmail.com>
2024-06-18 05:36:35 -0700

    m

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 function add(a, b) {
-  console.log(a + b);
+  if (isNaN(a) || isNaN(b)) {
+    console.log('NaN');
+  } else {
+    console.log(a + b);
+  }
 }
-add(parseInt(process.argv[2]), parseInt(process.argv[3]));
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+add(arg1, arg2);
