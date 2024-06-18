adf40a7b4a0c487a9eb4a301c99ab1b83a3791ca
mennaramadan9 <mennaramadan56@gmail.com>
2024-06-18 05:10:55 -0700

    m

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing size');
+} else {
+  const size = process.argv[2];
+  for(let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
