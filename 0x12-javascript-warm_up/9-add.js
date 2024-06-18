e4d763091ccfc028fc03c180baeb3d67594414b2
mennaramadan9 <mennaramadan56@gmail.com>
2024-06-18 05:38:55 -0700

    m

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-function add(a, b) {
-  console.log(a + b);
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
 }
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
-add(arg1, arg2);
+
+add(Number(process.argv[2]), Number(process.argv[3]));
